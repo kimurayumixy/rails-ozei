fa19578a9703e207a0a6651dd2d3bc8a6b986536
Yumi Kimura <kimurayumixy@gmail.com>
2022-11-21 18:04:58 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:index, :show]
+  resources :bookings, only: [:index, :create, :update]
+  namespace :owner do
+    resources :bookings, only: [:index, :update]
+  end
 end
