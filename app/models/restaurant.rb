f2d3fc08915a0429acd4559396becc6365e38683
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-11-21 15:19:52 +0900

    added restaurant model

---
+++ app/models/restaurant.rb
@@ -0,0 +1,4 @@
+class Restaurant < ApplicationRecord
+  has_many_attached :photos
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_033001) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_060048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "category"
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
+    t.boolean "availability"
+    t.integer "maximum_number"
+    t.string "price_range"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +37,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_033001) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
 end
