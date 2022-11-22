3a93df388ce19a1afc48a83ff2cc363bff2edd90
Erika Ura <erika255ura@gmail.com>
2022-11-22 14:30:53 +0900

    basic restaurant show and landing page no need to be logged in

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,22 @@
 class RestaurantsController < ApplicationController
-skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
+  before_action :set_restaurant, only: [:show]
 
   def index
     @restaurants = policy_scope(Restaurant)
   end
 
+  def show
+    authorize @restaurant
+  end
+
+  private
+
+  def restaurant
+    params.require(:restaurant).permit(:name)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
