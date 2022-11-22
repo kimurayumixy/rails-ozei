68bef15e636c0c385f2319c062e682ea4d2dbade
Soren Umstot <soren.umstot@gmail.com>
2022-11-22 11:05:11 +0900

    initial commit for restaurants model update

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,8 @@
 class Restaurant < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many_attached :photos
   belongs_to :user
-<<<<<<< HEAD
-
-=======
->>>>>>> fd3c78172bf885c8843884d7463d1d5cadf74824
   validates :name, presence: true
   validates :category, presence: true
   validates :address, presence: true, uniqueness: true
