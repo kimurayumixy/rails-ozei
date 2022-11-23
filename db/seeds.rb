ee28c97507f25d21bd733b62dae66e6a8da37299
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-11-23 11:40:06 +0900

    Added categories in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,13 +82,14 @@ end
 
 def create_restaurants
   puts "Building restaurants 🚧"
+  categories = ["All you can eat", "All you can drink", "Japanese, Kaiseki, Washoku", "Sushi", "Soba", "Tempura", "Yakiniku", "Steak, Teppanyaki", "Yakitori", "French", "Italian, Trattoria", "Western Cousine", "Chinese", "Bar", "Pub", "Izakaya"]
   seed_addresses = ADDRESSES.shuffle
   index = 0
   40.times do
     restaurant = Restaurant.create!(
       user: User.all.sample,
       name: Faker::Restaurant.name,
-      category: Faker::Food.ethnic_category,
+      category: categories.sample,
       address: seed_addresses[index],
       maximum_number: rand(1..30),
       price_range: ["¥", "¥¥", "¥¥¥", "¥¥¥¥"].sample
