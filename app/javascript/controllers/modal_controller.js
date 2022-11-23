c87e00f07631f9122c58d2192efb40f15066f221
Soren Umstot <soren.umstot@gmail.com>
2022-11-23 17:12:02 +0900

    added stimulus modal for our map

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,14 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ["content"]
+  static targets = ["modalContainer"]
   connect() {
     console.log("Modal controller connected")
-    console.log(this.contentTarget)
+    console.log(this.Target)
   }
 
-  revealMap(){
-    console.log(this.contentTarget)
-    this.contentTarget.classList.remove("modal-hide")
+  // revealMap(){
+  //   console.log(this.contentTarget)
+  //   this.contentTarget.classList.remove("modal-hide")
+  // }
+
+  launchTest() {
+    console.log(this.modalContainerTarget)
+    this.modalContainerTarget.classList.toggle('modal-hide')
   }
+//   open() {
+//     console.log("The modal has been opened!");
+// }
 }
