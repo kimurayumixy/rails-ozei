64e304ce42c710beb59b455fb4fd352b7bbf399a
Yumi Kimura <kimurayumixy@gmail.com>
2022-11-26 14:58:14 +0900

    notification still not working.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,9 @@ import "bootstrap"
 ////////////////////////////////////
 // MAP MODAL FUNCTIONALITY //
 
+// notification
+import { Application } from '@hotwired/stimulus'
+import Notification from 'stimulus-notification'
+
+const application = Application.start()
+application.register('notification', Notification)
