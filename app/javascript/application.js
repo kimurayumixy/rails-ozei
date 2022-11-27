aa364a8a51383247128e5dc6a0f2037b29658b71
Yumi Kimura <kimurayumixy@gmail.com>
2022-11-27 09:27:10 +0900

    time added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,10 +5,3 @@ import "bootstrap"
 
 ////////////////////////////////////
 // MAP MODAL FUNCTIONALITY //
-
-// notification
-import { Application } from '@hotwired/stimulus'
-import Notification from 'stimulus-notification'
-
-const application = Application.start()
-application.register('notification', Notification)
