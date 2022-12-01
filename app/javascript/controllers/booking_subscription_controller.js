c021b5a9ce570ba75e543dc7268206863b27f745
Yumi Kimura <kimurayumixy@gmail.com>
2022-12-01 11:59:49 +0900

    pending message

--- app/javascript/controllers/booking_subscription_controller.js
+++ app/javascript/controllers/booking_subscription_controller.js
@@ -3,18 +3,26 @@ import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
   static values = { bookingId: Number }
-  static targets = ["bookings"]
+  static targets = ["bookings", "card"]
   connect() {
-    console.log(this.bookingsTarget);
+    // console.log(this.bookingsTarget);
     this.channel = createConsumer().subscriptions.create(
       { channel: "BookingChannel", id: this.bookingIdValue },
       { received: data => {
-        console.log(this.bookingsTarget);
-        console.log(data);
+        // console.log(this.bookingsTarget);
+        console.log(data.status);
         this.bookingsTarget.innerHTML = data
+        console.log(this.bookingStatus);
+        if (this.bookingStatus === "restaurant_rejected") {
+          document.getElementById("booking-" + this.bookingIdValue).classList.add("opacity-50")
+        } else {
+          document.getElementById("booking-" + this.bookingIdValue).classList.remove("opacity-50")
+        }
+
+        // this.cardTarget.classList.add("opacity-50")
         }
       }
     )
-    console.log(`Subscribed to the booking with the id ${this.bookingIdValue}.`)
+    // console.log(`Subscribed to the booking with the id ${this.bookingIdValue}.`)
   }
 }
