545a43b734a126714983daccc3511bfedc90d61a
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-01 18:45:40 +0900

    added toggle on owners-page.

--- app/javascript/controllers/toggler_controller.js
+++ app/javascript/controllers/toggler_controller.js
@@ -2,6 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggler"
 export default class extends Controller {
+  static targets = [ "input" ]
   connect() {
+    console.log("HEY TOGGLER HERE!!!")
+  }
+
+  toggle() {
+    if (this.inputTarget.checked) {
+      const element = document.getElementById("main-content");
+      element.classList.add("gray-scale-all")
+    } else {
+      const element = document.getElementById("main-content");
+      element.classList.remove("gray-scale-all")
+    }
   }
 }
